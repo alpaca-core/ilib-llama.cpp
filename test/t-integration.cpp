1c8e690a7cab1918e1fd2051f76ae132d642f4f6
Plamen Minev <pacominev@gmail.com>
2024-11-27 18:17:46 +0200

    fix: move model registry to be hidden as a subclass of the model, ref #4

--- test/t-integration.cpp
+++ test/t-integration.cpp
@@ -15,8 +15,6 @@ struct GlobalFixture {
     GlobalFixture() {
         ac::llama::initLibrary();
     }
-
-    ac::llama::ModelRegistry modelRegistry;
 };
 
 GlobalFixture globalFixture;
@@ -24,8 +22,7 @@ GlobalFixture globalFixture;
 const char* Model_117m_q6_k = AC_TEST_DATA_LLAMA_DIR "/gpt2-117m-q6_k.gguf";
 
 TEST_CASE("vocab only") {
-    ac::llama::ModelRegistry mRegistry;
-    ac::llama::Model model = mRegistry.loadModel(Model_117m_q6_k, {}, {}, { .vocabOnly = true });
+    ac::llama::Model model(Model_117m_q6_k, {}, {}, { .vocabOnly = true });
     CHECK(!!model.lmodel());
 
     auto& params = model.params();
@@ -43,8 +40,7 @@ TEST_CASE("vocab only") {
 }
 
 TEST_CASE("inference") {
-    ac::llama::ModelRegistry mRegistry;
-    ac::llama::Model model = mRegistry.loadModel(Model_117m_q6_k, {}, {}, {});
+    ac::llama::Model model(Model_117m_q6_k, {}, {}, {});
     CHECK(!!model.lmodel());
 
     auto& params = model.params();
