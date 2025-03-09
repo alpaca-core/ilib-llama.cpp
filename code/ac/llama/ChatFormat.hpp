8ae1f8049c84a02487556c9af7d311b8805a7eb2
Plamen Minev <pacominev@gmail.com>
2025-03-09 13:46:19 +0200

    feat: support for jinja templates, ref #11
    -- it seems like we'll need to support legacy chat formatting because some of the templates are using addiational special tokens which we cannot get from the vocab, at least not right now. They have same issue in llama.cpp.

--- code/ac/llama/ChatFormat.hpp
+++ code/ac/llama/ChatFormat.hpp
@@ -10,38 +10,40 @@
 
 struct llama_chat_message;
 
+namespace minja {
+class chat_template;
+}
 namespace ac::llama {
+class Model;
+
 class AC_LLAMA_EXPORT ChatFormat {
 public:
-    // the template string here can be either an id or a markup
-    explicit ChatFormat(std::string tpl);
+    struct Params {
+        std::string chatTemplate;
+        std::string bosToken;
+        std::string eosToken;
+    };
 
-    // create a custom jinja template
-    // it should have a loop over 'messages' field
-    // each message must have a 'role' and 'content' field
-    explicit ChatFormat(std::string tpl, std::span<std::string> roles);
+    explicit ChatFormat(Params params);
 
-    const std::string& tpl() const noexcept { return m_template; }
-    const char* templateId() const noexcept;
+    ~ChatFormat();
+
+    const std::string& tpl() const noexcept { return m_templateStr; }
 
     // wrapper around llama_chat_apply_template
     // throw an error on unsupported template
     std::string formatChat(std::span<const ChatMsg> chat, bool addAssistantPrompt);
 
-    // wrapper around jinja template formatting
-    // params are options for the template
-    // TODO: Do not expose jinj2::ValuesMap to the user
-    // std::string formatChat(std::span<const ChatMsg> chat, jinja2::ValuesMap params = {});
-
     // format single message taking history into account
     std::string formatMsg(const ChatMsg& msg, std::span<const ChatMsg> history, bool addAssistantPrompt = false);
-
-    static std::vector<const char*> getSupportedTemplates();
 
 private:
     std::string apply(std::span<const llama_chat_message> chat, size_t chatSize, bool addAssistantPrompt) const;
 
-    std::string m_template;
+    std::string m_templateStr;
     int m_templateId;
+    std::unique_ptr<minja::chat_template> m_minjaTemplate;
 };
+
+ChatFormat::Params getChatParams(const Model& model);
 } // namespace ac::llama
