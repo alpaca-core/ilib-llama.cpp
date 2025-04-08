8ce947bfc3b852c9f2920bb87f377f3c20a9dd76
Plamen Minev <pacominev@gmail.com>
2025-04-08 18:08:24 +0300

    feat: PoC for sending multiple messages, ref #65

--- ac-local-plugin/example/ep-chat.cpp
+++ ac-local-plugin/example/ep-chat.cpp
@@ -31,7 +31,8 @@ int main() try {
     std::cout << "Initial state: " << sid << '\n';
 
     for (auto x : llama.stream<schema::StateLlama::OpLoadModel>({
-        .ggufPath = AC_TEST_DATA_LLAMA_DIR "/gpt2-117m-q6_k.gguf"
+        // .ggufPath = AC_TEST_DATA_LLAMA_DIR "/gpt2-117m-q6_k.gguf"
+        .ggufPath = AC_TEST_DATA_LLAMA_DIR "/../../../tmp/Meta-Llama-3.1-8B-Instruct-Q6_K.gguf"
     })) {
         std::cout << "Model loaded: " << x.tag.value() << " " << x.progress.value() << '\n';
     }
@@ -42,10 +43,22 @@ int main() try {
     sid = llama.call<schema::StateModelLoaded::OpStartInstance>({
         .instanceType = "chat",
         .setup = "A chat between a human user and a helpful AI assistant.",
-        .roleUser = roleUser,
-        .roleAssistant = roleAssistant
+        // .roleUser = roleUser,
+        // .roleAssistant = roleAssistant
     });
     std::cout << "Instance started: " << sid << '\n';
+
+    std::vector<schema::Message> initMessages = {
+        {roleUser, "I need assistance for API design"},
+        {roleAssistant, "What aspect of API design are you looking for help with? Do you have a specific problem or question in mind?"},
+        {roleUser, "It's a C++ implementation of a class"},
+    };
+
+    llama.call<schema::StateChatInstance::OpSendMessages>({
+        .messages = initMessages
+    });
+
+    std::vector<schema::Message> messages;
 
     while (true) {
         std::cout << roleUser <<": ";
@@ -55,19 +68,26 @@ int main() try {
         }
         if (user == "/quit") break;
         user = ' ' + user;
+        messages.push_back({roleUser, user});
+
         llama.call<schema::StateChatInstance::OpAddChatPrompt>({
             .prompt = user
         });
 
+        std::string text;
         std::cout << roleAssistant << ": ";
         constexpr bool streamChat = false;
         if (streamChat) {
             for(auto t: llama.stream<schema::StateChatInstance::OpStreamChatResponse>({})) {
+                text += t;
                 std::cout << t << std::flush;
             }
         } else {
-            std::cout << llama.call<schema::StateChatInstance::OpGetChatResponse>({}).response.value() << std::flush;
+            auto res = llama.call<schema::StateChatInstance::OpGetChatResponse>({});
+            text += res.response.value();
+            std::cout << res.response.value() << std::flush;
         }
+        messages.push_back({roleUser, text});
         std::cout << "\n";
     }
 
